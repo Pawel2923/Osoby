9c819b9344da92559d4f7bfd73a74659aa6d6aaa
Paweł Poremba <pawelporemba123@gmail.com>
2024-01-05 23:03:55 +0100

    Add project files.

---
+++ Osoby/Data/KontaktyContext.cs
@@ -0,0 +1,15 @@
+﻿using Osoby.Models;
+using System.Data.Entity;
+
+namespace Osoby.DAL
+{
+    public class KontaktyContext : DbContext
+    {
+        public KontaktyContext() : base("KontaktyConnectionString")
+        {
+
+        }
+
+        public DbSet<Kontakt> Kontakty { get; set; }
+    }
+}
---
+++ Osoby/Models/Kontakt.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Osoby.Models
+{
+    public class Kontakt
+    {
+        public int KontaktId { get; set; }
+        [Required(ErrorMessage="Wymagane podanie imienia")]
+        public string Imie { get; set; }
+        [Required(ErrorMessage = "Wymagane podanie nazwiska")]
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+    }
+}
