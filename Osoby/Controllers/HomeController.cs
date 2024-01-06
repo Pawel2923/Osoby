373ee8829f50c3cf748bdf8023ffbc85dc13b627
Paweł Poremba <pawelporemba123@gmail.com>
2024-01-06 12:49:29 +0100

    Add changes

--- Osoby/Controllers/HomeController.cs
+++ Osoby/Controllers/HomeController.cs
@@ -18,6 +18,64 @@ namespace Osoby.Controllers
             return View();
         }
 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Osoba osoba)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Osoba osoba)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Osoba osoba)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Read()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read(Osoba osoba)
+        {
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Privacy()
         {
             return View();
